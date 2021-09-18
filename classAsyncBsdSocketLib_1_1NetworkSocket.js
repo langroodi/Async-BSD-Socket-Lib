7f88ad61b9be5888879a75d822259b09156823d2
langroodi <langroodi@users.noreply.github.com>
2021-09-18 20:34:20 +0000

    Auto commit.

---
+++ classAsyncBsdSocketLib_1_1NetworkSocket.js
@@ -0,0 +1,12 @@
+var classAsyncBsdSocketLib_1_1NetworkSocket =
+[
+    [ "NetworkSocket", "classAsyncBsdSocketLib_1_1NetworkSocket.html#aadcfb2a634d12ac50526b2848f55345b", null ],
+    [ "~NetworkSocket", "classAsyncBsdSocketLib_1_1NetworkSocket.html#ad78d41fd9d9c42a31006d399c9857628", null ],
+    [ "Connection", "classAsyncBsdSocketLib_1_1NetworkSocket.html#a358b1b689cb7c3c44fdd24f1dd122cce", null ],
+    [ "Descriptor", "classAsyncBsdSocketLib_1_1NetworkSocket.html#ac631a12972720f106b08df19bf602b1a", null ],
+    [ "TryClose", "classAsyncBsdSocketLib_1_1NetworkSocket.html#a2e8ca9e7899e690d9e66d02279f6a441", null ],
+    [ "TrySetup", "classAsyncBsdSocketLib_1_1NetworkSocket.html#a0a61fa8891e29b5dba9491e124a0cca6", null ],
+    [ "mDescriptor", "classAsyncBsdSocketLib_1_1NetworkSocket.html#af6f3fae96cd8e1d0f821eb31a48b6927", null ],
+    [ "mIpAddress", "classAsyncBsdSocketLib_1_1NetworkSocket.html#a153ed67599d1c740d69e5688048d49e5", null ],
+    [ "mPort", "classAsyncBsdSocketLib_1_1NetworkSocket.html#a405d2faa615148f26377571ec0e755e3", null ]
+];
---
+++ search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['isconnected_32',['IsConnected',['../classAsyncBsdSocketLib_1_1TcpClient.html#a9b32c89b6633f7391eb829a03c2ffee5',1,'AsyncBsdSocketLib::TcpClient']]]
+];
