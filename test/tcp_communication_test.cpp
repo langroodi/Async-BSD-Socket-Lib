07217e2434c86a313f12674281aa03371253b19b
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2021-09-21 22:34:47 +0200

    Add TCP send/receive complementary test.

--- test/tcp_communication_test.cpp
+++ test/tcp_communication_test.cpp
@@ -75,4 +75,27 @@ namespace AsyncBsdSocketLib
                 std::cbegin(_receiveBuffer));
         EXPECT_TRUE(_areEqual);
     }
+
+    TEST_F(TcpCommunicationTest, ClientSendServerReceive)
+    {
+        const std::size_t cBufferSize = 16;
+
+        const std::array<uint8_t, cBufferSize> _sendBuffer{
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+
+        bool _sent = Client.Send<cBufferSize>(_sendBuffer) > 0;
+        EXPECT_TRUE(_sent);
+
+        std::array<uint8_t, cBufferSize> _receiveBuffer;
+        bool _received = Server.Receive<cBufferSize>(_receiveBuffer) > 0;
+        EXPECT_TRUE(_received);
+
+        bool _areEqual =
+            std::equal(
+                std::cbegin(_sendBuffer),
+                std::cend(_sendBuffer),
+                std::cbegin(_receiveBuffer));
+        EXPECT_TRUE(_areEqual);
+    }
 }
